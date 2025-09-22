78be45ccefd6ea2acc7675ccfeb65b183a6d3c4e
petar97 <petar97m@gmail.com>
2025-09-22 15:10:44 +0200

    Remove favicon setting from app.vue to streamline code and improve maintainability.

--- app/app.vue
+++ app/app.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script setup>
-// Set favicon
 useHead({
   link: [
     { rel: 'icon', type: 'image/x-icon', href: 'https://meridian.mpanel.app/image/cache/original/files/images/meridian-logo.png?crop=true' }
