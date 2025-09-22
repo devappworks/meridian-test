7f466bc3a40f06a06943e0a08e070d86921514e0
petar97 <petar97m@gmail.com>
2025-09-22 14:11:17 +0200

    Add favicon support in app.vue
    - Implemented favicon using useHead to enhance branding and user experience.
    - Linked favicon to the specified URL for consistent application identity across browsers.

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,12 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+// Set favicon
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: 'https://meridian.mpanel.app/image/cache/original/files/images/meridian-logo.png?crop=true' }
+  ]
+})
+</script>
