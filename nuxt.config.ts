3e920392647b698a4e244584e9361184ac6814df
petar97 <petar97m@gmail.com>
2025-09-23 09:47:40 +0200

    Update Google Tag Manager integration in nuxt.config.ts
    - Added new Google Tag Manager script for tracking with updated ID.
    - Modified existing GTM script to use the new ID for consistency.
    - Enhanced tracking capabilities by ensuring proper script loading order.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,12 +38,20 @@ export default defineNuxtConfig({
     head: {
       script: [
         // Google Tag Manager - must be first in head
+        { src: "https://www.googletagmanager.com/gtag/js?id=G-D36YF7TZJF" },
+        {innerHTML: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-D36YF7TZJF')`,
+          type: 'text/javascript'
+        },
         { 
           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NCRSG3GB');`,
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','G-D36YF7TZJF');`,
           type: 'text/javascript'
         },
         { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4', crossorigin: 'anonymous', tagPosition: 'bodyClose' },
