ee1205baea8541a2c63f5029ae4d62a2da12d652
petar97 <petar97m@gmail.com>
2025-10-07 17:38:35 +0200

    Refactor font loading strategy in nuxt.config.ts to enhance performance. Critical fonts are now loaded immediately with display=swap, while less critical fonts are deferred to improve rendering speed.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,27 +109,19 @@ export default defineNuxtConfig({
           media: 'print',
           onload: "this.media='all'"
         },
-        // Preload critical fonts with display=swap to prevent render blocking
+        // Critical fonts - load immediately with display=swap for better performance
         {
-          rel: 'preload',
-          as: 'style',
+          rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300;400;500;700&display=swap'
         },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&family=Roboto+Condensed:wght@300;400;500;700&display=swap',
-          media: 'print',
-          onload: "this.media='all'"
+          href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
         },
+        // Less critical fonts - defer loading
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap',
-          media: 'print',
-          onload: "this.media='all'"
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
           media: 'print',
           onload: "this.media='all'"
         },
